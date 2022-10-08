3a28d7050b9d1962f90eb9ec722b8d207450bd42
bintangyandiii <bintang.203040037@mail.unpas.ac.id>
2022-10-08 18:28:54 +0700

    menyelesaikan video 5(view)

--- phpmvc/app/controllers/About.php
+++ phpmvc/app/controllers/About.php
@@ -1,11 +1,21 @@
 <?php 
 
-class About {
-    public function index($nama ='Bintang', $pekerjaan ='Mahasiswa'){
-        echo "Hallo nama saya $nama, saya adalah seorang $pekerjaan";
+class About extends Controller {
+    public function index($nama ='Bintang', $pekerjaan ='Mahasiswa',$umur ='20'){
+        $data['judul']='About me';
+        $data['nama']=$nama;
+        $data['pekerjaan']=$pekerjaan;
+        $data['umur']=$umur;
+        $this->view('templates/hader',$data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
 
+
     public function page(){
-        echo 'About/page';
+        $data['judul']= 'Page';
+        $this->view('templates/hader',$data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
