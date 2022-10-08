7d2f856797080d6c947fc328c7747149c84c5161
bintangyandiii <bintang.203040037@mail.unpas.ac.id>
2022-10-08 19:53:51 +0700

    menyelesaikan video6 (assets)

--- phpmvc/app/views/about/index.php
+++ phpmvc/app/views/about/index.php
@@ -1,2 +1,9 @@
-<h1>About Me</h1>
+<div class="container">
+
+
+    <h1>About Me</h1>
+    <img src="<?=BASEURL;?>/img/bintang.jpg" alt="Bintang" width="150" class="rounded-circle shadow" >
     <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+
+
+</div>
