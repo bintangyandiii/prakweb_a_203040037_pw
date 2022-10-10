0c105cb29041eb4cb421b7d9bc993c604ea993a6
bintangyandiii <acutyp9@gmail.com>
2022-10-10 14:22:27 +0700

    memperbaiki bug

--- phpmvc/app/controllers/About.php
+++ phpmvc/app/controllers/About.php
@@ -6,7 +6,7 @@ class About extends Controller {
         $data['nama']=$nama;
         $data['pekerjaan']=$pekerjaan;
         $data['umur']=$umur;
-        $this->view('templates/hader',$data);
+        $this->view('templates/header',$data);
         $this->view('about/index', $data);
         $this->view('templates/footer');
     }
@@ -14,7 +14,7 @@ class About extends Controller {
 
     public function page(){
         $data['judul']= 'Page';
-        $this->view('templates/hader',$data);
+        $this->view('templates/header',$data);
         $this->view('about/page');
         $this->view('templates/footer');
     }
